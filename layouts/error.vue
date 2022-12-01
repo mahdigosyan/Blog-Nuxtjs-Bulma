89ec5cce9af9217f15ebc67d551da3669f5a00c6
Mahdi gosyan <mahdigosyanapple@gmail.com>
2022-12-01 23:38:14 +0330

    Create error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar />
+    <p class="title has-text-centered mt-3">
+      صفحه مورد نظر یافت نشد
+    </p>
+    <NuxtLink to="/">
+      <div class="has-text-centered mb-3">
+        <button class="button is-info">
+          بازگشت به صفحه اصلی
+        </button>
+      </div>
+    </NuxtLink>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+  layout: 'error',
+  props: {
+    error: {
+      type: String,
+      default: 'error'
+    }
+  }
+}
+</script>
