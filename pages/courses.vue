bb3d13c5448900366b790dc122d247a4731bd38f
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-03-16 23:15:16 +0330

    Create courses.vue

---
+++ pages/courses.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="has-text-centered">
+      <p>به زودی این قسمت تکمیل می شود</p>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoursesPage'
+}
+</script>
