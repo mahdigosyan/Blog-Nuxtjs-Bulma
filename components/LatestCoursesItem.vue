6ea6b230552a6e725b0406a34ed08feee24d0ffe
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-02 06:00:42 +0330

    Create LatestCoursesItem.vue

---
+++ components/LatestCoursesItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="box mt-1">
+    <article class="media">
+      <div class="media-content">
+        <div class="content">
+          <h2 class="has-text-right">
+            دوره شماره 1
+          </h2>
+          
