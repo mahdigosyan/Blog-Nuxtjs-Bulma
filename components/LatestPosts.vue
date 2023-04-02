f1ed10976374985e22d9dd83fc6c12e957e22639
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-02 06:05:08 +0330

    Update LatestPosts.vue

--- components/LatestPosts.vue
+++ components/LatestPosts.vue
@@ -4,4 +4,5 @@
       <div class="column">
         <LatestPostsItem :img="articles[0].img" :title="articles[0].title" :path="articles[0].path" :description="articles[0].description" />
       </div>
+      </div>
       
