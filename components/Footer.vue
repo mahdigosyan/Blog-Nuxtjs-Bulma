511550e2b1c88952e96048988b97f7b476c5ca86
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-02 05:51:54 +0330

    Create Footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        ساخته شده توسط مهدی قوسیان
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent'
+}
+</script>
+
+<style scoped>
+</style>
