087a37c99a4e02d8bda796cf97fb0ca315a37f57
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-04 05:10:37 +0330

    Update LatestPostsItem.vue

---
+++ components/LatestPostsItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box mt-1">
+      <article class="media">
+        <div class="media-content">
+          <div class="content">
+            <h2 class="has-text-right">
+              {{ title }}
+            </h2>
+            <div>
+              <p class="has-text-right">
+                {{ description }}
+              </p>
+            </div>
+          </div>
+          <NuxtLink :to="path">
+            <button class="button is-dark is-centered">
+              ادامه مطلب
+            </button>
+          </NuxtLink>
+        </div>
+        
